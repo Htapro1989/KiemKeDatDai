5468cb4704bd352f3dc5cdcd9a4c3dffc2c00311
htapro <htapr@HTAPRO>
2025-03-05 01:28:50 +0700

    anhht-update kỳ thống kê kiểm kê

---
+++ aspnet-core/src/KiemKeDatDai.Core/AppCore/DanhMucKyKiemKe/Dto/DMKyKiemKeDto.cs
@@ -0,0 +1,23 @@
+﻿using Abp.AutoMapper;
+using KiemKeDatDai.AppCore.Dto;
+using KiemKeDatDai.EntitiesDb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiemKeDatDai.Dto
+{
+    [AutoMap(typeof(KyThongKeKiemKe))]
+    public class DMKyKiemKeInputDto : KyThongKeKiemKe
+    {
+    }
+    public class DMKyKiemKeDto : PagedAndFilteredInputDto
+    {
+        public long Year { get; set; }
+    }
+    public class DMKyKiemKeOuputDto : KyThongKeKiemKe
+    {
+    }
+}
