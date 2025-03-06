0da4837b4deb474a40dae0ca67d03db482b233ea
htapro <htapr@HTAPRO>
2025-03-07 02:41:50 +0700

    anhht-api huyện

---
+++ aspnet-core/src/KiemKeDatDai.Application/App/Huyen/HuyenAppService.cs
@@ -0,0 +1,123 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.Authorization;
+using Abp.Authorization.Users;
+using Abp.AutoMapper;
+using Abp.Collections.Extensions;
+using Abp.Dependency;
+using Abp.Domain.Repositories;
+using KiemKeDatDai;
+using Abp.Domain.Uow;
+using Abp.Extensions;
+using Abp.Linq.Extensions;
+using Abp.ObjectMapping;
+using Abp.Runtime.Caching;
+using Abp.Threading;
+using KiemKeDatDai.Sessions;
+using KiemKeDatDai.ApplicationDto;
+using KiemKeDatDai.Authorization.Users;
+using KiemKeDatDai.Dto;
+using KiemKeDatDai.EntitiesDb;
+using KiemKeDatDai.Users;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Threading.Tasks;
+using KiemKeDatDai.RisApplication;
+
+namespace KiemKeDatDai.App.DMBieuMau
+{
+    public class HuyenAppService : KiemKeDatDaiAppServiceBase, IHuyenAppService
+    {
+        private readonly ICacheManager _cacheManager;
+        private readonly IIocResolver _iocResolver;
+        private readonly IRepository<KyThongKeKiemKe, long> _dmKyThongKeKiemKeRepos;
+        private readonly IRepository<DonViHanhChinh, long> _dvhcRepos;
+        private readonly IRepository<Bieu01TKKK_Huyen, long> _bieu01TKKK_HuyenRepos;
+        private readonly IRepository<Bieu01TKKK_Xa, long> _bieu01TKKK_XaRepos;
+        private readonly IRepository<User, long> _userRepos;
+        private readonly IObjectMapper _objectMapper;
+        private readonly IUserAppService _iUserAppService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IRepository<UserRole, long> _userRoleRepos;
+        //private readonly ILogAppService _iLogAppService;
+
+        private readonly ICache mainCache;
+
+        public HuyenAppService(ICacheManager cacheManager,
+            IIocResolver iocResolver,
+            IRepository<KyThongKeKiemKe, long> dmKyThongKeKiemKeRepos,
+            IRepository<DonViHanhChinh, long> dvhcRepos,
+            IRepository<Bieu01TKKK_Huyen, long> bieu01TKKK_HuyenRepos,
+            IRepository<Bieu01TKKK_Xa, long> bieu01TKKK_XaRepos,
+            IRepository<User, long> userRepos,
+            IObjectMapper objectMapper,
+            IUserAppService iUserAppService,
+            IRepository<UserRole, long> userRoleRepos,
+            IHttpContextAccessor httpContextAccessor
+            //ILogAppService iLogAppService
+            )
+        {
+            _dvhcRepos = dvhcRepos;
+            _dmKyThongKeKiemKeRepos = dmKyThongKeKiemKeRepos;
+            _bieu01TKKK_HuyenRepos = bieu01TKKK_HuyenRepos;
+            _bieu01TKKK_XaRepos = bieu01TKKK_XaRepos;
+            _objectMapper = objectMapper;
+            _iUserAppService = iUserAppService;
+            _httpContextAccessor = httpContextAccessor;
+            _userRoleRepos = userRoleRepos;
+            //_iLogAppService = iLogAppService;
+        }
+
+
+        [AbpAuthorize]
+        public async Task<CommonResponseDto> DuyetXa(long xaId)
+        {
+            CommonResponseDto commonResponseDto = new CommonResponseDto();
+            try
+            {
+                var currentUser = await GetCurrentUserAsync();
+                var objdata = await _dvhcRepos.FirstOrDefaultAsync(x => x.Id == currentUser.DonViHanhChinhId);
+                if (objdata != null)
+                {
+                    var xa = await _dvhcRepos.FirstOrDefaultAsync(x => x.Id == xaId);
+                    var data_xa = await _bieu01TKKK_XaRepos.FirstOrDefaultAsync(x => x.Id == xaId);
+                    if (data_xa != null)
+                    {
+                        await CreateOrUpdateBieu01TKKK_Huyen(data_xa, objdata.Id);
+                    }
+                    else
+                    {
+                        commonResponseDto.Message = "Dữ liệu xã không tồn tại";
+                        commonResponseDto.Code = CommonEnum.ResponseCodeStatus.ThatBai;
+                        return commonResponseDto;
+                    }
+                    commonResponseDto.Code = CommonEnum.ResponseCodeStatus.ThanhCong;
+                    commonResponseDto.Message = "Thành Công";
+                }
+                else
+                {
+                    commonResponseDto.Message = "Kỳ thống kê kiểm kê này không tồn tại";
+                    commonResponseDto.Code = CommonEnum.ResponseCodeStatus.ThatBai;
+                    return commonResponseDto;
+                }
+            }
+            catch (Exception ex)
+            {
+                commonResponseDto.Code = CommonEnum.ResponseCodeStatus.ThatBai;
+                commonResponseDto.Message = ex.Message;
+                throw;
+            }
+            return commonResponseDto;
+        }
+
+        private async Task CreateOrUpdateBieu01TKKK_Huyen(Bieu01TKKK_Xa xa, long huyenId)
+        {
+            var data_huyen = await _bieu01TKKK_HuyenRepos.GetAllListAsync(x => xa.Id == huyenId);
+        }
+    }
+}
