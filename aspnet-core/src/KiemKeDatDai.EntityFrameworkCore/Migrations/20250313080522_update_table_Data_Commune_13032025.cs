5f3d2f359cfbaeff60c283255a5a9fca8dafdd48
htapr <htapr@HTAPRO>
2025-03-13 15:07:12 +0700

    anhht-update db table Data_Commune

---
+++ aspnet-core/src/KiemKeDatDai.EntityFrameworkCore/Migrations/20250313080522_update_table_Data_Commune_13032025.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KiemKeDatDai.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_table_Data_Commune_13032025 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Geo",
+                table: "Data_Commune",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Geo",
+                table: "Data_Commune");
+        }
+    }
+}
