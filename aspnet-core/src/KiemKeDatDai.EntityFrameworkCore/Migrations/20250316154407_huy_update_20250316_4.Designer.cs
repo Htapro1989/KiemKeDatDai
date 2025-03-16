2ad1afa1118879929bf90b1dc7a7cb47077ab8ea
Quang Huy IT <huybq811@gmail.com>
2025-03-16 22:45:10 +0700

    huy_update_2025_03_16_7

--- aspnet-core/src/KiemKeDatDai.EntityFrameworkCore/Migrations/20250316154407_huy_update_20250316_4.Designer.cs
+++ aspnet-core/src/KiemKeDatDai.EntityFrameworkCore/Migrations/20250316154407_huy_update_20250316_4.Designer.cs
@@ -4,6 +4,7 @@ using KiemKeDatDai.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KiemKeDatDai.Migrations
 {
     [DbContext(typeof(KiemKeDatDaiDbContext))]
-    partial class KiemKeDatDaiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250316154407_huy_update_20250316_4")]
+    partial class huy_update_20250316_4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ aspnet-core/src/KiemKeDatDai.EntityFrameworkCore/Migrations/20250316154407_huy_update_20250316_4.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KiemKeDatDai.Migrations
+{
+    /// <inheritdoc />
+    public partial class huy_update_20250316_4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NguoiGocVietNamONuocNgoai_CNN_DT",
+                table: "Bieu04TKKK_Xa",
+                newName: "NguoiGocVietNamONuocNgoai_NGV_DT");
+
+            migrationBuilder.RenameColumn(
+                name: "NguoiGocVietNamONuocNgoai_CNN_CC",
+                table: "Bieu04TKKK_Xa",
+                newName: "NguoiGocVietNamONuocNgoai_NGV_CC");
+
+            migrationBuilder.RenameColumn(
+                name: "NguoiGocVietNamONuocNgoai_CNN_DT",
+                table: "Bieu04TKKK_Vung",
+                newName: "NguoiGocVietNamONuocNgoai_NGV_DT");
+
+            migrationBuilder.RenameColumn(
+                name: "NguoiGocVietNamONuocNgoai_CNN_CC",
+                table: "Bieu04TKKK_Vung",
+                newName: "NguoiGocVietNamONuocNgoai_NGV_CC");
+
+            migrationBuilder.RenameColumn(
+                name: "NguoiGocVietNamONuocNgoai_CNN_DT",
+                table: "Bieu04TKKK_Tinh",
+                newName: "NguoiGocVietNamONuocNgoai_NGV_DT");
+
+            migrationBuilder.RenameColumn(
+                name: "NguoiGocVietNamONuocNgoai_CNN_CC",
+                table: "Bieu04TKKK_Tinh",
+                newName: "NguoiGocVietNamONuocNgoai_NGV_CC");
+
+            migrationBuilder.RenameColumn(
+                name: "NguoiGocVietNamONuocNgoai_CNN_DT",
+                table: "Bieu04TKKK_Huyen",
+                newName: "NguoiGocVietNamONuocNgoai_NGV_DT");
+
+            migrationBuilder.RenameColumn(
+                name: "NguoiGocVietNamONuocNgoai_CNN_CC",
+                table: "Bieu04TKKK_Huyen",
+                newName: "NguoiGocVietNamONuocNgoai_NGV_CC");
+
+            migrationBuilder.RenameColumn(
+                name: "NguoiGocVietNamONuocNgoai_CNN_DT",
+                table: "Bieu04TKKK",
+                newName: "NguoiGocVietNamONuocNgoai_NGV_DT");
+
+            migrationBuilder.RenameColumn(
+                name: "NguoiGocVietNamONuocNgoai_CNN_CC",
+                table: "Bieu04TKKK",
+                newName: "NguoiGocVietNamONuocNgoai_NGV_CC");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NguoiGocVietNamONuocNgoai_NGV_DT",
+                table: "Bieu04TKKK_Xa",
+                newName: "NguoiGocVietNamONuocNgoai_CNN_DT");
+
+            migrationBuilder.RenameColumn(
+                name: "NguoiGocVietNamONuocNgoai_NGV_CC",
+                table: "Bieu04TKKK_Xa",
+                newName: "NguoiGocVietNamONuocNgoai_CNN_CC");
+
+            migrationBuilder.RenameColumn(
+                name: "NguoiGocVietNamONuocNgoai_NGV_DT",
+                table: "Bieu04TKKK_Vung",
+                newName: "NguoiGocVietNamONuocNgoai_CNN_DT");
+
+            migrationBuilder.RenameColumn(
+                name: "NguoiGocVietNamONuocNgoai_NGV_CC",
+                table: "Bieu04TKKK_Vung",
+                newName: "NguoiGocVietNamONuocNgoai_CNN_CC");
+
+            migrationBuilder.RenameColumn(
+                name: "NguoiGocVietNamONuocNgoai_NGV_DT",
+                table: "Bieu04TKKK_Tinh",
+                newName: "NguoiGocVietNamONuocNgoai_CNN_DT");
+
+            migrationBuilder.RenameColumn(
+                name: "NguoiGocVietNamONuocNgoai_NGV_CC",
+                table: "Bieu04TKKK_Tinh",
+                newName: "NguoiGocVietNamONuocNgoai_CNN_CC");
+
+            migrationBuilder.RenameColumn(
+                name: "NguoiGocVietNamONuocNgoai_NGV_DT",
+                table: "Bieu04TKKK_Huyen",
+                newName: "NguoiGocVietNamONuocNgoai_CNN_DT");
+
+            migrationBuilder.RenameColumn(
+                name: "NguoiGocVietNamONuocNgoai_NGV_CC",
+                table: "Bieu04TKKK_Huyen",
+                newName: "NguoiGocVietNamONuocNgoai_CNN_CC");
+
+            migrationBuilder.RenameColumn(
+                name: "NguoiGocVietNamONuocNgoai_NGV_DT",
+                table: "Bieu04TKKK",
+                newName: "NguoiGocVietNamONuocNgoai_CNN_DT");
+
+            migrationBuilder.RenameColumn(
+                name: "NguoiGocVietNamONuocNgoai_NGV_CC",
+                table: "Bieu04TKKK",
+                newName: "NguoiGocVietNamONuocNgoai_CNN_CC");
+        }
+    }
+}
