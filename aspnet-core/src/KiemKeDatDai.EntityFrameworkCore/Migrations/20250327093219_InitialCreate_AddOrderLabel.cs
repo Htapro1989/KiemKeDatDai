9ba5d364e0d25d163beaee8041ae55a82eb83571
XuanCuongDo <cuongdx88@gmail.com>
2025-03-27 16:34:14 +0700

    add migration for order label

---
+++ aspnet-core/src/KiemKeDatDai.EntityFrameworkCore/Migrations/20250327093219_InitialCreate_AddOrderLabel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KiemKeDatDai.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate_AddOrderLabel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OrderLabel",
+                table: "News",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderLabel",
+                table: "News");
+        }
+    }
+}
