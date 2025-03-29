808f1c78fcc9a5864e4b00d6753f5c7d3056aa66
xuancuongdo <cuongdx988@gmail.com>
2025-03-29 16:59:48 +0700

    add migration for DienTichMucDich

---
+++ aspnet-core/src/KiemKeDatDai.EntityFrameworkCore/Migrations/20250329094832_MakeDienTichMucDichOptional.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KiemKeDatDai.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeDienTichMucDichOptional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "DienTichMucDich",
+                table: "Data_Commune",
+                type: "decimal(18,4)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,4)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "DienTichMucDich",
+                table: "Data_Commune",
+                type: "decimal(18,4)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,4)",
+                oldNullable: true);
+        }
+    }
+}
