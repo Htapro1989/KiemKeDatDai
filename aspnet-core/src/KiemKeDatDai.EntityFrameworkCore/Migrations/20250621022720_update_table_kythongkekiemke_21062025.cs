36f214df0437102b0df9890a396ae7bd5f859309
htapro <htapr@HTAPRO>
2025-06-21 09:29:16 +0700

    anhht - update db

---
+++ aspnet-core/src/KiemKeDatDai.EntityFrameworkCore/Migrations/20250621022720_update_table_kythongkekiemke_21062025.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KiemKeDatDai.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_table_kythongkekiemke_21062025 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LoaiCapDVHC",
+                table: "KyThongKeKiemKe",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LoaiCapDVHC",
+                table: "KyThongKeKiemKe");
+        }
+    }
+}
